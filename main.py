42bf201dc71f81f1cf47a63e308eb42c37df5a05
Muksini Mkombe <muksinimkombe23@gmail.com>
2026-03-29 15:28:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ def chat():
 
     return jsonify({"reply": reply})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
