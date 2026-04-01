c9cb6a89409dbc2e16d4e277a2bcb594ff4d414a
Muksini Mkombe <muksinimkombe23@gmail.com>
2026-04-01 11:22:22 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -7,11 +7,27 @@ technology_used = "Python (Flask, Pandas, NumPy, OpenAI)"
 creation_year = "2026"
 purpose = "Helpful assistant in Mzumbe PastPaper System"
 
+def bot_rules():
+    return f""""
+        Rules in answering the question
+        -consise
+        -very shortly
+        -avoid long explanation
+        -one line recomendation
+        -pin point to the question
+    """
+    
+
 def bot_response():
     return f"""
     Your name is {name_of_model}.
     You were created by {creator_of_model}.
     You were built using {technology_used}.
     You were created in {creation_year}.
-    Your purpose is: {purpose}.
+    Your purpose is: {purpose}.  
+    Rules in answering the question are : {bot_rules()}
     """
+
+
+
+
